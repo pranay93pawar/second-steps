19986895f3b31da48d6c342032bc7bbff9de3d4f
Pranay Pawar <pranay93pawar@gmail.com>
2017-06-19 23:55:08 +0530

    First Commit

---
+++ DigitReader/DigitReader/ShowArt.swift
@@ -0,0 +1,16 @@
+//
+//  ShowArt.swift
+//  DigitReader
+//
+//  Created by Pranay Suresh Pawar on 19/06/17.
+//  Copyright © 2017 Pranay Suresh Pawar. All rights reserved.
+//
+
+import Foundation
+
+class ShowArt {
+    var showTitle:String = String()
+    var showLink:String = String()
+    
+    
+}
