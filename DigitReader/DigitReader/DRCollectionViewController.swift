50220ba5686128d8ced3d493b549fb372f45f6bf
Pranay Pawar <pranay.pawar@resilinc.com>
2017-08-28 12:12:36 +0530

    reordering of collectionview

--- DigitReader/DigitReader/DRCollectionViewController.swift
+++ DigitReader/DigitReader/DRCollectionViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
+fileprivate let sectionInsets = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 50.0, right: 20.0)
+fileprivate let itemsPerRow: CGFloat = 2
+
 class DRCollectionViewController: UICollectionViewController {
 
     private let reuseIdentifier = "CollectionViewCell"
-    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    fileprivate let itemsPerRow: CGFloat = 2
     var feedItems:[FeedItem] = []
     
     override func viewDidLoad() {
@@ -118,4 +119,28 @@ class DRCollectionViewController: UICollectionViewController {
     }
     */
 
+}
+
+extension DRCollectionViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return sectionInsets.left
+    }
+    
 }
